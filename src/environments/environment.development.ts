2b2b1d565fff0d84718dff3250de174ee6d04efa
zGIKS <166082577+zGIKS@users.noreply.github.com>
2026-05-16 02:13:12 -0500

    feat: refactor Stripe public key handling to fetch from backend configuration

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: '/api',
-  stripePublicKey: 'pk_test_51TSju6FuLTQlQKp58xPZzsCUjI4pL8KTZWVyyarMJNB8VmVgJVkSsTAbMwuMcMD51Oz2PtJsAihrMmn6F3Z7j8XD00R9I7GFqT'
+  apiBaseUrl: '/api'
 } as const;
-
